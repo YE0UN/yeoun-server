42387801a02994a376e5f41ab479905819edfcfc
민경 <tjalsrud0101@gmail.com>
2023-07-24 15:01:17 +0900

    feat: 지역별 관광지

--- routes/tours.js
+++ routes/tours.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, query } = require('express');
 const router = Router();
 
 const tourInfo = require('../utils/tour-info');
@@ -8,9 +8,19 @@ const asyncHandler = require('../utils/async-handler');
 router.get('/', asyncHandler(async (req, res) => {
     const {region} = req.query;
     var result = [];
-    tourInfo.forEach(function(tour, index, array) {
-        result.push(tour);
+
+    // 전체 관광지 정보
+    if(!region) {
+        return res.json(tourInfo);
+    }
+
+    // 지역별 관광지 정보
+    tourInfo.forEach(function(tour) {
+        if(JSON.stringify(tour).includes(region)) {
+            result.push(tour);
+        }
     });
+    
     res.json(result);
   }));
 
--- utils/tour-info.js
+++ utils/tour-info.js
@@ -1,7 +1,8 @@
 const request = require('request');
 const convert = require('xml-js');
 
-var url = 'http://api.data.go.kr/openapi/tn_pubr_public_trrsrt_api';
+// 공공 API에서 관광지 정보 받아오기
+const url = 'http://api.data.go.kr/openapi/tn_pubr_public_trrsrt_api';
 const serviceKey = 'lzQ%2BGyumiJBpbEBhDNX6WO9WoTJ7leGpYVrHXLhNQiYI1eoBlgLWcuIp%2FMp02Vdnv4PIIKHgsksULew1PYczyg%3D%3D';
 var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey;
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1000');
@@ -15,8 +16,11 @@ request({
     if(error) {
         console.log(error)
     }
+    // xml파일 Json 형태 변환 후 파싱
     var xmlToJson = convert.xml2json(body, {compact: true, spaces: 4});
     var parsedJson = JSON.parse(xmlToJson);
+
+    // 관광지명, 위치, 소개 정보
     for(i in parsedJson['response']['body']['items']['item']) {
         tourData.push({
             name : parsedJson['response']['body']['items']['item'][i]['trrsrtNm']['_text'],
@@ -24,7 +28,6 @@ request({
             introduction : parsedJson['response']['body']['items']['item'][i]['trrsrtIntrcn']['_text']
         })
     }
-    console.log(tourData);
 });
 
 module.exports = tourData;
