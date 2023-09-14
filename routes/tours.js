ffbb0ae156aed36f262d0c8ffa737d4d553e3156
민경 <tjalsrud0101@gmail.com>
2023-09-15 00:14:24 +0900

    Revert "refactor: 지역 선택 에러 처리"

--- routes/tours.js
+++ routes/tours.js
@@ -3,25 +3,21 @@ const router = Router();
 
 const tourInfo = require('../utils/tour-info');
 const asyncHandler = require('../utils/async-handler');
-const statusCode = require("../utils/status-code");
 
 
 router.get('/', asyncHandler(async (req, res) => {
     const {region} = req.query;
 
-    if (!region) {
-        return res.status(statusCode.BAD_REQUEST).json({error: "지역을 선택하세요."});
-    }
+    // 조건을 만족하는 관광지만 필터링
+    const filteredTours = region
+        ? tourInfo.filter((tour) => {
+            const location = JSON.stringify(tour.location).split(' ')[0];
+            return location.includes(region);
+        })
+        : null;
 
-    // 지역에 맞는 관광지 필터링
-    const filteredTours = tourInfo.filter((tour) => {
-        const location = JSON.stringify(tour.location).split(' ')[0];
-        return location.includes(region);
-    });
-
-    res.json(filteredTours);
-}));
-
+    res.json(filteredTours || tourInfo);
+  }));
 
 
 module.exports = router;
